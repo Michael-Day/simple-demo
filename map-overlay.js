d944084a381a58cb94205bf441cc61d0afed274b
mday <mday@mpd3.home>
2016-01-09 13:17:06 +0000

    Some colour changes

--- map-overlay.js
+++ map-overlay.js
@@ -1,18 +1,21 @@
-
+/*
+Map scroll and hands
+7th January 2016
+*/
 
 function setup() {
-  var canvass = createCanvas(windowWidth,windowHeight);
+  var canvass = createCanvas(600,600);
   canvass.position(0, 0, P2D);
 
 }
-  var myX = 0;
+var myX = 0;
 function draw() {
-  background(80,80,80);
-
+  //  background(80,80,80, 0.1);
+  clear();
   stroke(255);
   line(myX,0,myX,windowHeight);
   myX++;
-  if(myX > windowWidth){
+  if(myX > width){
     myX = 0;
 
   }
