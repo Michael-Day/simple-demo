726864f0433898c82c730cc80e0310cf9bd9a536
Michael-Day <mday@mpd3.local>
2016-01-26 15:44:58 +0000

    Changes to texts, fades, and layout
    Updated the text database and moved to a separate file. Added fade outs
    and different timings to the texts. Started to organise a range of
    locations.

--- map-overlay.js
+++ map-overlay.js
@@ -1,96 +1,115 @@
 /*
 Map scroll thing
-19th January 2016
+23rd January 2016
 
 TO DO
-type sizes
-colours
-bigger text database
-upload to server
+
 fade out, fade back in again
+sound
+upload to server
+working on android/ios
 */
-var symptom; //
+
+var symptom; // div for the texts
+var fadeOut; // var for the fadeout function if text is unclicked
+
+// var object containing the lat and long for the locations to zoom from
+var locs =
+    {
+        ny:[40.70531887544228, -74.00976419448853],
+        oldst: [51.52569,-0.08736],
+        apple: [37.332342, -122.030797]
+    }
 
 function setup() {
-    var canvass = createCanvas(1,1);
-    //canvass.position(300, 200, P2D);
-    symptom = createSpan();
-    symptom.id("text");
-    symptom.html(randomText());
-    windowResized();
+    var canvass = createCanvas(1,1);    // canvas, for no reason
+
+    symptom = createSpan();         // span for the texts
+    symptom.id("text");             // give it an id, defined in the HTML file
+    symptom.addClass("visible");    // give it a class, visible for the fades etc
+
+    symptom.html(randomText());     // generate text content for the div
+    windowResized();                // flow for smaller screens
+
+    symptom.touchEnded(makeInvisible);      // set up listeners for the clicks and taps
+    symptom.mouseReleased(makeInvisible);
+
+
 }
-var myX = 0;
-var axiss = 0;
+
+
+// This is all mapzen / leaflet business //
 var mapp = L.map('map', {
-    zoomControl: false,       // gets rid of +/- thing
+    zoomControl: false,        // gets rid of +/- thing
     attributionControl: false, // gets rid of attribution thing
 });
-
 var layer = Tangram.leafletLayer({
     scene: 'scene.yaml',
     //attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
 });
-
 layer.addTo(mapp);
 
-// process is to centre and zoom out
-mapp.setView([51.52569,-0.08736], 19); // old st
-//mapp.setView([40.70531887544228, -74.00976419448853], 15); //new york
-panMe = function(){
-    //mapp.panBy([-2,0]);
-    mapp.zoomOut(0.001);
+
+mapp.setView(locs.apple, 19); // This sets up the map position, to old st
+
+setInterval(refreshLocation, 360000);
+
+// function to do the zooming out
+zoomMe = function(){
+        mapp.zoomOut(0.001);
+
 }
-setInterval(panMe, 100);
+setInterval(zoomMe, 100);
 
 function windowResized() {
-  // sets the type size depending on the window size
-  // mod this to not overflow the text box
+  // sets the type box size depending on the window size
   if(symptom){
-    if(windowWidth > 1000){
-      symptom.style("font-size","3.6em");
-    } else if (windowWidth > 500) {
-      symptom.style("font-size","2.1em");
-    } else if (windowWidth < 499) {
-      symptom.style("font-size","1.4em");
-    }
+      if(windowWidth > 1000){
+          symptom.style("font-size","2.8em");
+          symptom.style("width", "40%");
+      } else if (windowWidth > 800) {
+          symptom.style("font-size","2.2em");
+          symptom.style("width", "50%");
+      } else if (windowWidth > 500) {
+          symptom.style("font-size","2.0em");
+          symptom.style("width", "60%");
+      } else if (windowWidth < 499) {
+          symptom.style("font-size","1.3em");
+          symptom.style("width", "80%");
+      }
   }
 }
 
-// this is the p5js stuff //
-var textArray = new Array(" You are attached to your smartphone.",
-    "You default to screen time in your downtime.",
-    "You're afraid of silence.",
-    "Phone calls aren't your communication of preference.",
-    "You believe in multitasking.",
-    "You can’t make up your mind.",
-    "You grab the closest device when left alone.",
-    "You’re worried you’re missing out.",
-    "Being away from your phone makes you anxious.",
-    "Your self-esteem has taken a hit.",
-    "You sleep with your phone.",
-    "You feel less connected.",
-    "Social media makes you feel negative about your own life.",
-    "You check your email and/or social feeds throughout every meal.",
-    "You can’t function when your phone dies.",
-    "You keep bumping into things.",
-    "You can’t make up your mind.",
-    "Your vision goes blurry after looking at a screen.",
-    "You never read an article from beginning to end.",
-    "Your thumb on your dominant hand is sore from all the scrolling.",
-    "You look at your phone first thing when you wake up.",
-    "You get a bit twitchy and anxious if separated from your phone.",
-    "You get panicked when the battery is running low.",
-    "You are feeling hurried and lacking in concentration."
-);
 
-// var xe = Math.random(textArray.length);
-// xe = Math.round(xe);
-// var s = textArray[xe];
-
-randomText = function(){
+function randomText() {
+    // generates the text in the text box
+    // textArray is in a separate js file
     xe = Math.random()*textArray.length;
     xe = Math.round(xe);
     s = textArray[xe];
     symptom.html(s);
+
+    // set the visibility of the text to visible
+    var thing =  document.getElementById("text"); // get the element
+    thing.className = "visible";                 // change the class of it
+
+    // start counter for text fadeout if unclicked
+    fadeOut = setInterval(makeInvisible, 10400);
 }
-setInterval(randomText, 5000);
+
+setInterval(randomText, 12000); // refresh interval for texts
+
+
+function makeInvisible() {
+    // fades out texts on click
+    clearTimeout(fadeOut);      // clear the timer for UNclicked fadeout
+    var thing =  document.getElementById("text"); // get the element
+    thing.className = "hidden";         // change the class of it
+    console.log("clicked");             // debug
+}
+
+function refreshLocation() {
+    // work up through the list of locations
+    mapp.setView(locs.ny, 19); // This sets up the map position, to old st
+
+}
